01292220127428ef12e17b003867265f4476b2ab
Mauricio Soria <mauricioutn2017@gmail.com>
2023-06-06 17:09:37 -0300

    Agregada esctructura principal del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from './components/Footer'
+import './App.css'
+import Principal from './components/Principal'
+
+function App() {
+
+  return (
+    <>
+    <Principal></Principal>
+    <Footer></Footer>
+    </>
+  )
+}
+
+export default App
