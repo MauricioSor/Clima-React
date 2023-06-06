b61f6d98e2f9c9d679ef434737759157014cb636
Mauricio Soria <mauricioutn2017@gmail.com>
2023-06-06 20:30:32 -0300

    conexion con api y  consulta exitosa

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,37 @@
-import Footer from './components/Footer'
+import Footer from './components/Footer';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
 import Principal from './components/Principal'
 import { useEffect, useState } from 'react'
 
 function App() {
 const [clima,setClima]= useState([]);
-
+useEffect(()=>{
+  consultarAPI();
+  },[]);
 const consultarAPI = async(e)=>{
   if(e&& e!=='Tucuman'){
+    let apikey='1f998e9fb087014c08d45fe5301f583a'
     const consultar = await fetch(`https://api.openweathermap.org/data/2.5/weather?country=Argentina&q=${e.target.value}&lang=sp&appid=${apikey}`);
-
+    const respuesta = await consultar.json();
+    setClima(respuesta);
+    console.log(respuesta);
+    console.log(clima);
   }else{
     try{
       let apikey='1f998e9fb087014c08d45fe5301f583a'
-      const consultar = await fetch(`https://api.openweathermap.org/data/2.5/weather?country=Argentina&q=Tucuman&lang=sp&appid=${apikey}`);
+      const consultar = await fetch(`https://api.openweathermap.org/data/2.5/weather?country=Argentina&q=Tucuman&lang=es&appid=${apikey}`);
       const respuesta = await consultar.json();
-      const respuestaArray= Object.values(respuesta);
-      setClima(respuestaArray);
-      console.log(respuestaArray);
+      setClima(respuesta);
+      console.log(respuesta);
+      console.log(clima);
     }catch(error){
       throw(Error);
     }
   }
 
 }
-useEffect(()=>{
-consultarAPI();
-},[])
+
   return (
     <>
     <Principal clima={clima}></Principal>
--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import { Container } from 'react-bootstrap';
 
 const Principal = ({clima}) => {
-    return (
-        <div className='d-flex'>
-        <div className='container'>
+    
+    console.log(clima.weather[0].description);
+    const temperatura = (clima.main.temp)-(273.15);
+    const temperaturaMin = (clima.main.temp_min)-(273.15);
+    const temperaturaMax = (clima.main.temp_max)-(273.15);
 
-        </div>
-        <div className='container'>
-        <p></p>
-        </div>
+    return (
+        <div className='d-flex justify-content-center border circle-border'>
+        <Container>
+        </Container>
+        <Container>
+        <p>{clima.weather[0].description}</p>
+        <p>Temperatura: <strong>{temperatura}°</strong></p>
+        <p>Temperatura Minima: <strong>{temperaturaMin}°</strong></p>
+        <p>Temperatura Minima: <strong>{temperaturaMax}°</strong></p>
+        <p>Humedad: <strong>{clima.main.humidity}%</strong></p>
+        <p>Viento: <strong>{clima.wind.speed}m/s</strong></p>
+        </Container>
         </div>
     );
 };
