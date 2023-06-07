7a8113b804df7113b8e5ff011e8565f95802257f
Mauricio Soria <mauricioutn2017@gmail.com>
2023-06-06 22:32:26 -0300

    funcionando codigo punto cumplido

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,24 @@ import Footer from './components/Footer';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
 import Principal from './components/Principal'
+import { Container,Button } from 'react-bootstrap';
 import { useEffect, useState } from 'react'
+import Form from 'react-bootstrap/Form';
 
 function App() {
 const [clima,setClima]= useState({});
 
-const consultarAPI = async(e)=>{
-  if(e&& e!=='Tucuman'){
+
+const tomaDeDatos = (e)=>{
+  e.preventDefault();
+  const ciudad = e.target.elements[0].value;
+  consultarAPI(ciudad);
+  setClima('');
+}
+const consultarAPI = async(ciudad)=>{
+  if(ciudad&& ciudad!=='Tucuman'){
     let apikey='1f998e9fb087014c08d45fe5301f583a'
-    const consultar = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&lang=sp&appid=${apikey}`);
+    const consultar = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&lang=sp&appid=${apikey}`);
     const respuesta = await consultar.json();
     setClima(respuesta);
     console.log(respuesta);
@@ -31,10 +40,23 @@ const consultarAPI = async(e)=>{
 useEffect(()=>{
   consultarAPI();
   },[]);
+
   return (
     <>
     <h1 className='my-4 text-center'>Estado del Clima</h1>
     <hr />
+    <Container fluid className='d-flex justify-content-center'>
+            <h3 className='text-center me-2'>Ingresar nombre de ciudad</h3>
+            <Form className="d-flex text-center" onSubmit={tomaDeDatos}>
+                <Form.Control
+                    type="search"
+                    placeholder="Pj. Salta"
+                    className="me-2"
+                    aria-label="Search"
+                />
+                <Button type="submit"variant="outline-success">Buscar</Button>
+            </Form>
+            </Container>
     <Principal clima={clima}></Principal>
     <Footer></Footer>
     </>
--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -10,19 +10,22 @@ const Principal = ({ clima }) => {
     const temperatura = (clima.main.temp) - 273.15;
     const temperaturaMin = (clima.main.temp_min) - 273.15;
     const temperaturaMax = (clima.main.temp_max) - 273.15;
-
+    
     return (
-        <div className='d-flex justify-content-center border my-3'>
-            <Container className='text-center my-2'>
-                <video src={CieloClaro} controls />
-            </Container>
-            <Container className='my-2'>
-                <p>{clima.weather[0].description}</p>
-                <p>Temperatura: <strong>{temperatura}°</strong></p>
-                <p>Temperatura Mínima: <strong>{temperaturaMin}°</strong></p>
-                <p>Temperatura Máxima: <strong>{temperaturaMax}°</strong></p>
-                <p>Humedad: <strong>{clima.main.humidity}%</strong></p>
-                <p>Viento: <strong>{clima.wind.speed} m/s</strong></p>
+        <div >
+            <Container className='d-flex justify-content-center border my-3'>
+                <Container className='text-center my-2'>
+                    <video src={CieloClaro} controls />
+                </Container>
+                <Container className='my-2'>
+                    <h2>{clima.name}</h2>
+                    <p>Estado: <strong>{clima.weather[0].description}</strong></p>
+                    <p>Temperatura: <strong>{temperatura}°</strong></p>
+                    <p>Temperatura Mínima: <strong>{temperaturaMin}°</strong></p>
+                    <p>Temperatura Máxima: <strong>{temperaturaMax}°</strong></p>
+                    <p>Humedad: <strong>{clima.main.humidity}%</strong></p>
+                    <p>Viento: <strong>{clima.wind.speed} m/s</strong></p>
+                </Container>
             </Container>
         </div>
     );
